f5320dfb28335fd3c1fb01000a176d71e26c4cba
bobdabuilderllc <59704499+bobdabuilderllc@users.noreply.github.com>
2020-01-15 09:20:47 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,89 @@
-let mySprite = sprites.create(img`
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 f f f 1 f 1 1 f 1 f f f 1 1 
+1 1 1 f 1 1 f f 1 f 1 1 f 1 1 1 
+1 1 1 f 1 1 f 1 f f 1 1 f 1 1 1 
+1 1 1 f 1 1 f 1 1 f 1 1 f 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, mySprite, 9999, 0)
+})
+controller.B.onEvent(ControllerButtonEvent.Released, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 f f f 1 f 1 1 f 1 f f f 1 1 
+1 1 1 f 1 1 f f 1 f 1 1 f 1 1 1 
+1 1 1 f 1 1 f 1 f f 1 1 f 1 1 1 
+1 1 1 f 1 1 f 1 1 f 1 1 f 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, mySprite, 9999, 0)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 f f f 1 f 1 1 f 1 f f f 1 1 
+1 1 1 f 1 1 f f 1 f 1 1 f 1 1 1 
+1 1 1 f 1 1 f 1 f f 1 1 f 1 1 1 
+1 1 1 f 1 1 f 1 1 f 1 1 f 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, mySprite, 9999, 0)
+})
+controller.A.onEvent(ControllerButtonEvent.Released, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 f f f 1 f 1 1 f 1 f f f 1 1 
+1 1 1 f 1 1 f f 1 f 1 1 f 1 1 1 
+1 1 1 f 1 1 f 1 f f 1 1 f 1 1 1 
+1 1 1 f 1 1 f 1 1 f 1 1 f 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, mySprite, 9999, 0)
+})
+info.onLifeZero(function () {
+    game.reset()
+})
+let projectile: Sprite = null
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
 . . . . 7 7 7 7 7 7 7 7 . . . . 
 . . . . 7 7 2 7 7 2 7 7 . . . . 
 . . . . 7 7 2 7 7 2 7 7 . . . . 
@@ -16,4 +101,9 @@ let mySprite = sprites.create(img`
 . . 7 7 7 7 . . . . 7 7 7 7 . . 
 . . 7 7 7 7 . . . . 7 7 7 7 . . 
 `, SpriteKind.Player)
-controller.moveSprite(mySprite, 100, 100)
+controller.moveSprite(mySprite, 200, 200)
+info.setScore(0)
+info.setLife(3)
+scene.setBackgroundColor(15)
+info.setScore(0)
+mySprite.setFlag(SpriteFlag.StayInScreen, true)
